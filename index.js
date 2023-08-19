50f20075eca6704bc63afd1633a19c548d7dbbff
Jayant pawar <pawarjayant80@gmail.com>
2023-08-19 21:57:58 +0530

    navbar and sidebar work done

---
+++ index.js
@@ -0,0 +1,6 @@
+let btn=document.getElementById("menu-btn")
+let sidebar=document.querySelector(".sidebar-container")
+btn.addEventListener('click',()=>{
+    sidebar.classList.toggle("small-sidebar") 
+    sidebar.document.style.color="black"   
+})
